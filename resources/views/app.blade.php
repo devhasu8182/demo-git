6071225fea76dde7dc91324ab625759fdd86c3b9
tom00072 <tom00072@protonmail.com>
2021-12-27 17:17:46 +0530

    incomplete while issu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
     <title>Vue JS CRUD Operations in Laravel</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="{{ asset('assets/css/floating-labels.css') }}">
 </head>
 
 <body>
